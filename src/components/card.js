668f9b2257e57ae4fb35ce397dac42dcde59cd25
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2021-01-20 19:28:25 -0500

    card component

---
+++ src/components/card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Card(props) {
+    return (
+        <tr>
+            <th>
+                <img alt={props.firstName} src={props.picture} />
+            </th>
+            <td>{props.firstName}</td>
+            <td>{props.lastName}</td>
+            <td>{props.email}</td>
+            <td>{props.phone}</td>
+            <td>{props.city}</td>
+        </tr>
+    );
+}
+
+export default Card;
